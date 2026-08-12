ac48e1495f0ed1fa85f240e9b7e2e302c31c3c89
WHS-DTECH <vanessapringle@westlandhigh.school.nz>
2026-08-12 16:10:40 +1200

    Fix missing closing parenthesis in topicDisplayName ternary chain

--- ProjectPages/activity-detail.js
+++ ProjectPages/activity-detail.js
@@ -6739,7 +6739,7 @@ function renderDetailView(host, id, data, canEdit, selectedTaskTopic = "", selec
                 ? DIGITAL_OUTCOME_DEVELOPMENT_TOOLS_TITLE
                 : (preferredTemplateId === "project-success-criteria"
                     ? DIGITAL_OUTCOME_SUCCESS_CRITERIA_TITLE
-                    : (preferredTemplateId === "relevant-implications" ? DIGITAL_OUTCOME_RELEVANT_IMPLICATIONS_TITLE : DIGITAL_OUTCOME_DESCRIPTION_TITLE)));
+                    : (preferredTemplateId === "relevant-implications" ? DIGITAL_OUTCOME_RELEVANT_IMPLICATIONS_TITLE : DIGITAL_OUTCOME_DESCRIPTION_TITLE))));
         templateLibraryParams.set("preFilterTopic", topicDisplayName);
     }
     const slideshowTemplateLibraryUrl = `slideshow-template-library.html?${templateLibraryParams.toString()}`;
