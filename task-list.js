d70c3c145d1b0e8df5cfd64e3463f283c5bfe2df
WHS-DTECH <vanessapringle@westlandhigh.school.nz>
2026-08-27 09:37:38 +1200

    Show Conventions subtask badge on Merit conventions row too

--- task-list.js
+++ task-list.js
@@ -1844,7 +1844,7 @@ function renderChecklistCards(detail, allItems) {
                                     : getTaskTopicHrefForStep(standard, level, stepText);
                                 const isInformationalRow = isInformationalCriteriaRow(standard, level, stepText);
                                 const is91893ToolsAndTechniquesRow = /using appropriate tools and techniques for the purpose and end users/i.test(stepText);
-                                const is91893ConventionsRow = /using relevant conventions for the media type/i.test(stepText);
+                                const is91893ConventionsRow = /using relevant conventions for the media type|applying relevant conventions to improve the quality of the outcome/i.test(stepText);
                                 const is91893EfficientToolsRow = /using efficient tools and techniques in the outcome.?s production/i.test(stepText);
                                 const is91893IntegrityTestingRow = /applying appropriate data integrity and testing procedures/i.test(stepText);
                                 const conventionsSubtask = is91893ConventionsRow ? getDigiMedConventionsSubtask(taskListState.fullEvidenceState) : null;
